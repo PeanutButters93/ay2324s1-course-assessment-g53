b20ae6b70045cc1cd7f47b42bd7bac1f3c31b955
zzthian <zzthian@hotmail.com>
2023-09-02 23:20:54 +0800

    init app and landing page

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import "./App.css";
+
+const DEFAULT_QNS = [
+  {
+    id: 1,
+    title: "Reverse a String",
+    description:
+      "Write a function that reverses a string. The input string is given as an array of characters s.You must do this by modifying the input array in-place with O(1) extra memory.",
+    category: "Strings, Algorithms",
+    complexity: "EASY",
+  },
+  {
+    id: 2,
+    title: "Linked List Cycle Detection",
+    description:
+      "Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter. Return true if there is a cycle in the linked list. Otherwise, return false.",
+    category: "Data Structures, Algorithms",
+    complexity: "EASY",
+  },
+];
+
+function App() {
+  const [questions, setQuestions] = useState(DEFAULT_QNS);
+
+  return (
+    <>
+      <header>
+        <h1>Welcome to PeerPrep</h1>
+      </header>
+      <div>
+        <h2>
+          Here are some questions you can practice! Feel free to add more :)
+        </h2>
+        <table>
+          <tr>
+            <th>Question ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Complexity</th>
+          </tr>
+          {questions.map((x) => (
+            <tr>
+              <th>{x.id}</th>
+              <th>{x.title}</th>
+              <th>{x.description}</th>
+              <th>{x.category}</th>
+              <th>{x.complexity}</th>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default App;
