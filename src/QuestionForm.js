43ed0f91243d6a657aebdaf790834f70825181a0
kohkaixun <kkx1929@gmail.com>
2023-09-08 03:30:33 +0800

    Change default selection to "Please select an option"

--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -4,36 +4,60 @@ import "./QuestionForm.css";
 const QuestionForm = (props) => {
     const { addQuestion, COMPLEXITY } = props;
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [category, setCategory] = useState("");
-    const [complexity, setComplexity] = useState(COMPLEXITY.EASY);
+    const [complexity, setComplexity] = useState(null);
 
     return (
-        <form>
+        <form onSubmit = {handleSubmit}>
             <h2>Question Form</h2>
             <div>
-                <label htmlFor="title">Title: </label>
-                <input type="text" id="title" />
+                <label htmlFor = "title">Title: </label>
+                <input 
+                  type = "text" 
+                  id = "title" 
+                  value = { title } 
+                  onChange = {(event) => setTitle(event.target.value)}
+                />
             </div>
             <div>
-                <label htmlFor="description">Description: </label>
-                <textarea id="description" />
+                <label htmlFor = "description">Description: </label>
+                <textarea 
+                  id = "description" 
+                  value = { description } 
+                  onChange = {(event) => setDescription(event.target.value)}
+                />
             </div>
             <div>
-                <label htmlFor="category">Category: </label>
-                <input type="text" id="category" />
+                <label htmlFor = "category">Category: </label>
+                <input 
+                  type = "text" 
+                  id = "category" 
+                  value = { category } 
+                  onChange = {(event) => setCategory(event.target.value)}
+                />
             </div>
             <div>
-                <label htmlFor="complexity">Complexity: </label>
-                <select id="complexity">
-                    <option value="">--Please choose an option--</option>
-                    <option value="EASY">Easy</option>
-                    <option value="MEDIUM">Medium</option>
-                    <option value="HARD">Hard</option>
+                <label htmlFor = "complexity">Complexity: </label>
+                <select 
+                  id = "complexity"
+                  value={ complexity } 
+                  onChange = {(event) => setComplexity(event.target.value)}
+                >
+                    <option value = {null}>--Please choose an option--</option>
+                    {Object.values(COMPLEXITY).map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
                 </select>
             </div>
-            <button type="submit">Submit</button>
+            <button type = "submit">Submit</button>
         </form>
     )
 }
