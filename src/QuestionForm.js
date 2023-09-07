c7846a556e18c24e999577df31664594260e9b21
kohkaixun <kkx1929@gmail.com>
2023-09-08 00:23:40 +0800

    Add title, description, category and complexity textboxes

--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -1,10 +1,31 @@
+import "./QuestionForm.css";
+
 const QuestionForm = () => {
     return (
-        <div>
+        <form>
             <h2>Question Form</h2>
-            <label htmlFor="title">Title: </label>
-            <input type="text" id="title" />
-        </div>
+            <div>
+                <label htmlFor="title">Title: </label>
+                <input type="text" id="title" />
+            </div>
+            <div>
+                <label htmlFor="description">Description: </label>
+                <textarea id="description" />
+            </div>
+            <div>
+                <label htmlFor="category">Category: </label>
+                <input type="text" id="category" />
+            </div>
+            <div>
+                <label htmlFor="complexity">Complexity: </label>
+                <select id="complexity">
+                    <option value="EASY">Easy</option>
+                    <option value="MEDIUM">Medium</option>
+                    <option value="HARD">Hard</option>
+                </select>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
