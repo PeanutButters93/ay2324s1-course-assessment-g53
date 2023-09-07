0c9190ecae6c940cbbf7d2aa8c0bfee7bc149395
kohkaixun <kkx1929@gmail.com>
2023-09-08 00:40:13 +0800

    Change question complexity to enum Complexity was initially string of EASY, MEDIUM or HARD. Now is declared as enum.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import { useState } from "react";
 import "./App.css";
 import QuestionForm from "./QuestionForm";
 
+const COMPLEXITY = {
+    EASY: "EASY",
+    MEDIUM: "MEDIUM",
+    HARD: "HARD",
+};
+
 const DEFAULT_QNS = [
   {
     id: 1,
@@ -9,7 +15,7 @@ const DEFAULT_QNS = [
     description:
       "Write a function that reverses a string. The input string is given as an array of characters s. You must do this by modifying the input array in-place with O(1) extra memory.",
     category: "Strings, Algorithms",
-    complexity: "EASY",
+    complexity: COMPLEXITY.EASY,
   },
   {
     id: 2,
@@ -17,7 +23,7 @@ const DEFAULT_QNS = [
     description:
       "Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter. Return true if there is a cycle in the linked list. Otherwise, return false.",
     category: "Data Structures, Algorithms",
-    complexity: "EASY",
+    complexity: COMPLEXITY.EASY,
   },
 ];
 
