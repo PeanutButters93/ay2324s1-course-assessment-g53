5f7c9c64683650cb6f57bdac65b9ec5dc2ccc3d0
LowJF <lowjianfeng@hotmail.com>
2023-09-14 04:21:02 +0800

    Question : Add ability to style tooltip

--- frontend/src/components/Question.js
+++ frontend/src/components/Question.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Question.css";
 
+//Tooltip Solution adapted from https://www.w3schools.com/css/css_tooltip.asp
+
 const Question = ({ question_data }) => {
   console.log(question_data);
   return (
     <>
       <td>{question_data.id}</td>
-      <td title={question_data.description}>{question_data.title}</td>
+      <td class = "tooltip">
+        {question_data.title}
+        <span class="tooltiptext">{question_data.description}</span>
+      </td>
       <td>{question_data.category}</td>
       <td>{question_data.complexity}</td>
     </>
