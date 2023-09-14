94b585e175d3b4450d8e4005adfc5117a1745b06
kohkaixun <kkx1929@gmail.com>
2023-09-14 21:11:23 +0800

    Add dependencies for Dashboard.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,15 @@ import QuestionPage from "./pages/QuestionPage";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Layout from "./pages/Layout";
+import Dashboard from "./dashboard/Dashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
-        <Route index element={<QuestionPage />} />
+        {/* <Route index element={<QuestionPage />} /> */}
+        <Route index element={<Dashboard />} />
         <Route path="login" element={<Login />} />
         <Route path="logout" element={<Logout />} />
       </Routes>
