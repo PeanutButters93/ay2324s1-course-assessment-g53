710c2de289334a11ba444fbd00f17eee16292bd7
kohkaixun <kkx1929@gmail.com>
2023-09-14 23:28:05 +0800

    Split screen into two

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -89,95 +89,101 @@ const defaultTheme = createTheme({
 });
 
 export default function Dashboard() {
-  const [open, setOpen] = React.useState(true);
-  const toggleDrawer = () => {
-    setOpen(!open);
-  };
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
+    return (
+        <ThemeProvider theme={defaultTheme}>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar position="absolute" open={open}>
+            <Toolbar
+                sx={{
+                pr: '24px', // keep right padding when drawer closed
+                }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
+                <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                    marginRight: '36px',
+                    ...(open && { display: 'none' }),
+                }}
+                >
+                <MenuIcon />
+                </IconButton>
+                <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                sx={{ flexGrow: 1 }}
+                >
+                Dashboard
+                </Typography>
+                <IconButton color="inherit">
+                <Badge badgeContent={4} color="secondary">
+                    <NotificationsIcon />
+                </Badge>
+                </IconButton>
+            </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+            <Toolbar
+                sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1],
+                }}
             >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
+                <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+                </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+                {mainListItems}
+                <Divider sx={{ my: 1 }} />
+                {secondaryListItems}
+            </List>
+            </Drawer>
+            <Box
+            component="main"
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
+                backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                    ? theme.palette.grey[100]
+                    : theme.palette.grey[900],
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto',
             }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === 'light'
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: '100vh',
-            overflow: 'auto',
-          }}
-        >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Questions */}
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid>
-            </Grid>
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
+            >
+            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Grid container spacing={3}>
+                    {/* Questions */}
+                    <Grid item xs={12} md={6}>
+                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                            <Orders />
+                        </Paper>
+                    </Grid>
+                    {/*View Edit Question*/}
+                    <Grid item xs={12} md={6}>
+                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                            <Orders />
+                        </Paper>
+                    </Grid>
+                </Grid>
+                <Copyright sx={{ pt: 4 }} />
+            </Container>
+            </Box>
         </Box>
-      </Box>
-    </ThemeProvider>
-  );
+        </ThemeProvider>
+    );
 }
