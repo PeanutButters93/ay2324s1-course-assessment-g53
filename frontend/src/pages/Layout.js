03e7d3249df7f271fa63ce9bda562ad57e07d680
LowJF <lowjianfeng@hotmail.com>
2023-09-16 17:07:16 +0800

    Align sidebar to be beside pages, instead of above

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -52,27 +52,29 @@ const Layout = () => {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Drawer variant="permanent" open={open}>
-        <Toolbar
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-            px: [1],
-          }}
-        >
-          <IconButton onClick={toggleDrawer}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </Toolbar>
-        <Divider />
-        <List component="nav">
-          {mainListItems}
-          <Divider sx={{ my: 1 }} />
-          {secondaryListItems}
-        </List>
-      </Drawer>
-      <Outlet className="child" />
+      <div className="rowcontainer">
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-end",
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+        </Drawer>
+        <Outlet className="child" />
+      </div>
   </ThemeProvider>
   );
 };
