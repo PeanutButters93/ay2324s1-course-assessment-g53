25e8e9fef181db605bb2897614d3865eef41b260
LowJF <lowjianfeng@hotmail.com>
2023-09-17 17:55:00 +0800

    UserProfile : Change table to form

--- frontend/src/pages/UserProfile.js
+++ frontend/src/pages/UserProfile.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from "react";
 import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import PersonIcon from '@mui/icons-material/Person';
@@ -9,14 +10,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
+import { TextField, FormControl } from "@mui/material";
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme({
@@ -25,27 +19,30 @@ const defaultTheme = createTheme({
   },
 });
 
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-
 export default function UserProfile() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setEmailError(false)
+    setPasswordError(false)
+
+    if (email == '') {
+      setEmailError(true)
+    }
+    if (password == '') {
+      setPasswordError(true)
+    }
+
+    if (email && password) {
+      console.log(email, password)
+    }
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
@@ -96,35 +93,35 @@ export default function UserProfile() {
         </Box>
       </main>
 
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <h2>Personal Information</h2>
+        <TextField
+          label="Email"
+          onChange={e => setEmail(e.target.value)}
+          required
+          variant="outlined"
+          color="secondary"
+          type="email"
+          sx={{ mb: 3 }}
+          fullWidth
+          value={email}
+          error={emailError}
+        />
+        <TextField
+          label="Password"
+          onChange={e => setPassword(e.target.value)}
+          required
+          variant="outlined"
+          color="secondary"
+          type="password"
+          value={password}
+          error={passwordError}
+          fullWidth
+          sx={{ mb: 3 }}
+        />
+        <Button variant="outlined" color="secondary" type="submit">Login</Button>
+
+      </form>
       {/* Footer */}
       {/* End footer */}
     </ThemeProvider>
