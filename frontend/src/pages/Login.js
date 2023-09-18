d746e8eb52250dc0a32e4e7a26c78eeac77f9697
Darren Chang <darrenchang2@gmail.com>
2023-09-18 12:34:14 +0800

    Link the frontend and backend for signuppage and login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -57,25 +57,37 @@ function Login() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-        // Additional validation here
-        if (!isValidEmailOrUsername(userIdentifier)) {
-          alert('Please enter a valid email or username.');
-          return;
-        }
+    // Additional validation here
+    if (!isValidEmailOrUsername(userIdentifier)) {
+      alert('Please enter a valid email or username.');
+      return;
+    }
 
-        if (!isValidPassword(password)) {
-          setPasswordError('Password does not meet the requirements.');
-          return;
-        } else {
-          setPasswordError('');  // Clear the error
-        }
+    if (!isValidPassword(password)) {
+      setPasswordError('Password does not meet the requirements.');
+      return;
+    } else {
+      setPasswordError('');  // Clear the error
+    }
 
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-    navigate('/questionpage')
+    fetch('http://localhost:3000/api/users/getUser', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userIdentifier: userIdentifier,  // Use the userIdentifier state variable
+        password: password,              // Use the password state variable
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response from the backend. Maybe store a token or show a success message.
+        navigate('/questionpage');
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   };
 
   return (
@@ -93,57 +105,57 @@ function Login() {
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
-<Typography 
-    component="h1" 
-    variant="h4"
-    sx={{
-        fontWeight: 'bold',
-        marginTop: 2,
-        marginBottom: 2,
-        letterSpacing: '0.1rem',
-        textAlign: 'center', 
-        color: 'primary.main',
-        textShadow: '1px 1px 2px gray',
-        transition: 'color 0.3s, transform 0.3s',  // smooth transition for color and transform
-        cursor: 'pointer',  // make it look interactive
-        '&:hover': {
-            color: 'secondary.main',  // change color on hover
-            transform: 'scale(1.05)'  // slightly increase size on hover
-        }
-    }}
->
-    ~Welcome Back~
-</Typography>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{
+              fontWeight: 'bold',
+              marginTop: 2,
+              marginBottom: 2,
+              letterSpacing: '0.1rem',
+              textAlign: 'center',
+              color: 'primary.main',
+              textShadow: '1px 1px 2px gray',
+              transition: 'color 0.3s, transform 0.3s',  // smooth transition for color and transform
+              cursor: 'pointer',  // make it look interactive
+              '&:hover': {
+                color: 'secondary.main',  // change color on hover
+                transform: 'scale(1.05)'  // slightly increase size on hover
+              }
+            }}
+          >
+            ~Welcome Back~
+          </Typography>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="userIdentifier"
-          label="Email Address or Username" // Updated label
-          name="userIdentifier"
-          autoComplete="email"
-          value={userIdentifier}
-          onChange={e => setUserIdentifier(e.target.value)}  // Use the new state variable here
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Password"
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-        onChange={e => setPassword(e.target.value)}
-        error={!!passwordError}  // If there's an error message, this will be true
-        helperText={passwordError}  // Display the error message
-      />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="userIdentifier"
+              label="Email Address or Username" // Updated label
+              name="userIdentifier"
+              autoComplete="email"
+              value={userIdentifier}
+              onChange={e => setUserIdentifier(e.target.value)}  // Use the new state variable here
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              error={!!passwordError}  // If there's an error message, this will be true
+              helperText={passwordError}  // Display the error message
+            />
             <Button
               type="submit"
               fullWidth
--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -20,23 +20,23 @@ import { useState } from 'react';
 
 
 function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <MUILink color="inherit" href="https://mui.com/">
-        PeerPrep
-      </MUILink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <MUILink color="inherit" href="https://mui.com/">
+                PeerPrep
+            </MUILink>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 const darkTheme = createTheme({
     palette: {
-      mode: 'dark',  // This switches the theme mode to dark
+        mode: 'dark',  // This switches the theme mode to dark
     },
-  });
+});
 
 
 const isValidPassword = (password) => {
@@ -48,7 +48,7 @@ const isValidEmail = (email) => {
     const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     return re.test(email);
 }
-  function SignupPage() {
+function SignupPage() {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const [firstName, setFirstName] = useState('');
@@ -57,10 +57,6 @@ const isValidEmail = (email) => {
     const [isValid, setIsValid] = useState(true);
     const navigate = useNavigate();
 
-
-
-
-
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -68,172 +64,162 @@ const isValidEmail = (email) => {
             setIsValid(false);
             return; // Don't proceed further
         }
-        
-      const data = new FormData(event.currentTarget);
-      console.log({
-        email: data.get('email'),
-        password: data.get('password'),
-      });
-      navigate('/questionpage')
 
+        // Construct the user data
+        const userData = {
+            username: username,
+            email: email,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+
+        // Send the user data to the backend
+        fetch('http://localhost:3000/api/users/createUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Handle the response from the backend. Maybe show a success message or store some data.
+                navigate('/questionpage');
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Maybe show an error message to the user
+            });
     };
+
   
-//   const history = useHistory();
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     const data = new FormData(event.currentTarget);
 
-//     const userDetails = {
-//       firstname: data.get('firstName'),
-//       lastname: data.get('lastName'),
-//       username: data.get('username'),
-//       email: data.get('email'),
-//       password: data.get('password'),
-//     };
-
-//     // Send a POST request to the backend
-//     fetch('/createUser', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify(userDetails),
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//       // Handle the response from the backend. Maybe redirect or show a success message.
-//     })
-//     .catch(error => {
-//       console.error('Error:', error);
-//     });
-//};
-
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography 
-    component="h1" 
-    variant="h4"
-    sx={{
-        fontWeight: 'bold',
-        marginTop: 2,
-        marginBottom: 2,
-        letterSpacing: '0.1rem',
-        textAlign: 'center', 
-        color: 'primary.main',
-        textShadow: '1px 1px 2px gray',
-        transition: 'color 0.3s, transform 0.3s',  // smooth transition for color and transform
-        cursor: 'pointer',  // make it look interactive
-        '&:hover': {
-            color: 'secondary.main',  // change color on hover
-            transform: 'scale(1.05)'  // slightly increase size on hover
-        }
-    }}
->
-    Welcome to PeerPrep
-</Typography>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            autoComplete="given-name"
-            name="firstName"
-            required
-            fullWidth
-            id="firstName"
-            label="First Name"
-            onChange={e => setFirstName(e.target.value)}
-            autoFocus
-          />
-          {!firstName && <p>First name is required.</p>}
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            fullWidth
-            id="lastName"
-            label="Last Name"
-            name="lastName"
-            onChange={e => setLastName(e.target.value)}
-            autoComplete="family-name"
-          />
-          {!lastName && <p>Last name is required.</p>}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            onChange={e => setEmail(e.target.value)}
-          />
-          {!isValidEmail(email) && <p>Email is not valid.</p>}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            onChange={e => setUsername(e.target.value)}
-          />
-          {!username && <p>Username is required.</p>}
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="new-password"
-            onChange={e => setPassword(e.target.value)}
-          />
-          {!isValidPassword(password) && <p>Password does not meet the requirements.</p>}
-        </Grid>
-      </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-              <MUILink to="/login" component={RouterLink} variant="body2">
-                Already have an account? Sign in
-                </MUILink>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography
+                        component="h1"
+                        variant="h4"
+                        sx={{
+                            fontWeight: 'bold',
+                            marginTop: 2,
+                            marginBottom: 2,
+                            letterSpacing: '0.1rem',
+                            textAlign: 'center',
+                            color: 'primary.main',
+                            textShadow: '1px 1px 2px gray',
+                            transition: 'color 0.3s, transform 0.3s',  // smooth transition for color and transform
+                            cursor: 'pointer',  // make it look interactive
+                            '&:hover': {
+                                color: 'secondary.main',  // change color on hover
+                                transform: 'scale(1.05)'  // slightly increase size on hover
+                            }
+                        }}
+                    >
+                        Welcome to PeerPrep
+                    </Typography>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    onChange={e => setFirstName(e.target.value)}
+                                    autoFocus
+                                />
+                                {!firstName && <p>First name is required.</p>}
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    onChange={e => setLastName(e.target.value)}
+                                    autoComplete="family-name"
+                                />
+                                {!lastName && <p>Last name is required.</p>}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    onChange={e => setEmail(e.target.value)}
+                                />
+                                {!isValidEmail(email) && <p>Email is not valid.</p>}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="username"
+                                    label="Username"
+                                    name="username"
+                                    autoComplete="username"
+                                    onChange={e => setUsername(e.target.value)}
+                                />
+                                {!username && <p>Username is required.</p>}
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    onChange={e => setPassword(e.target.value)}
+                                />
+                                {!isValidPassword(password) && <p>Password does not meet the requirements.</p>}
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                <MUILink to="/login" component={RouterLink} variant="body2">
+                                    Already have an account? Sign in
+                                </MUILink>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 export default SignupPage;
