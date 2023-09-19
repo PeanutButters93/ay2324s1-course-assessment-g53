a875055f84218a510ebf743e836224d6deac11af
LowJF <lowjianfeng@hotmail.com>
2023-09-20 03:10:49 +0800

    Add AuthProvider to wrap BrowserRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,22 +8,35 @@ import UpdateUserProfilePage from "./pages/UpdateUserProfilePage"
 import SignupPage from "./pages/SignupPage"
 import { Link } from "react-router-dom"
 import AdminView from "./pages/AdminView"
+import { AuthProvider, RequireAuth } from 'react-auth-kit'
 
 
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<SignupPage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="logout" element={<Logout />} />
-        <Route path="updateProfile" element={<UpdateUserProfilePage />} />
-        <Route path="questionpage" element={<QuestionPage />} />
-        <Route path="adminview" element={<AdminView />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider
+      authType={"cookie"}
+      authName={"_auth"}
+      cookieDomain={window.location.hostname}
+      cookieSecure={true}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} />
+          <Route index element={<SignupPage />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logout" element={<Logout />} />
+          <Route path="updateProfile" element={<UpdateUserProfilePage />} />
+          {/* <Route path="questionpage" element={<QuestionPage />} /> */}
+          <Route
+            path="questionpage"
+            element={<RequireAuth loginPath="/login">
+            <QuestionPage />
+          </RequireAuth>} />
+          <Route path="adminview" element={<AdminView />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
