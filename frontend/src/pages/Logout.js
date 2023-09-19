cea17866ad1cbb4236d14d7313659702a8de5367
LowJF <lowjianfeng@hotmail.com>
2023-09-20 03:19:23 +0800

    Add abilitiy to remove cookie when visiting Logout (Should change to button)

--- frontend/src/pages/Logout.js
+++ frontend/src/pages/Logout.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSignOut } from "react-auth-kit";
+import { useNavigate } from "react-router-dom";
+
+// #TODO turn into a button
 
 const Logout = () => {
+  useSignOut();
+  // useNavigate("/login")
   return <div style={{ color: "white" }}>This is the logout page</div>;
 };
 
