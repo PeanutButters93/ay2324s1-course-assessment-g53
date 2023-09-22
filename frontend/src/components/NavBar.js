78c7171aeb387977a032d53632fccdfc54162903
LowJF <lowjianfeng@hotmail.com>
2023-09-22 21:31:29 +0800

    NavBar : Add Log Out button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,15 @@
 import List from '@mui/material/List'
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import DataObjectIcon from '@mui/icons-material/DataObject';
+import LogoutIcon from '@mui/icons-material/Logout';
 import PersonIcon from '@mui/icons-material/Person';
 import NavComponent from './NavComponent'
+import useCookie from './useCookie';
 
 const NavBar = () => {
+
+  const {clearCookies} = useCookie();
+
   return <List>
 
       <NavComponent
@@ -22,7 +27,13 @@ const NavBar = () => {
         icon = {<DataObjectIcon />}
         href = "../adminview"
       />
-
+      
+      <NavComponent
+        text = "Log Out"
+        icon = {<LogoutIcon />}
+        href = "../"
+        onClick={clearCookies}
+      />
   </List>;
 }
 
--- frontend/src/components/NavComponent.js
+++ frontend/src/components/NavComponent.js
@@ -2,8 +2,11 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 
-const NavComponent = ({text, icon, href}) => {
-  return <ListItemButton href={href}>
+const NavComponent = ({text, icon, href, onClick}) => {
+  return <ListItemButton
+    href={href}
+    onClick ={onClick}
+    >
     <ListItemIcon>
       {icon}
     </ListItemIcon>
