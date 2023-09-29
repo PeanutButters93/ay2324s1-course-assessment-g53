ed26b1444e9cd673aa28ce9f11514075cfcc3303
zzthian <zzthian@hotmail.com>
2023-09-30 01:43:44 +0800

    Get a working timer that performs a counting down

--- frontend/src/pages/Match.js
+++ frontend/src/pages/Match.js
@@ -1,22 +1,40 @@
 import { Button, Select , Box, FormControl, InputLabel, MenuItem} from "@mui/material";
-import { ThemeProvider } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+
 
 
 const Match = (props) => {
     const [difficulty, setDifficulty] = useState("")
+    const [timeLeft, setTimeLeft] = useState(30);
+    const [startCount, setStartCount] = useState(false)
 
     const handleDifficultyChange = (event) => {
         event.preventDefault()
         setDifficulty(event.target.value)
     }
 
+    useEffect(() => {
+        if (startCount) {
+            const intervalId = setInterval(() => {
+                setTimeLeft(timeLeft - 1);
+            }, 1000)
+            return () => {
+                clearInterval(intervalId)
+                if (timeLeft <= 0){
+                    setStartCount(false)
+                }
+            }
+        } else {
+            return;
+        }
+    }, [startCount, timeLeft])
+
     const handleSubmit = (event) => {
         event.preventDefault()
         console.log(difficulty)
+        setStartCount(true)
         console.log("BYEE")
     }
-
 
     return (
     <Box sx={{ display: "flex", justifyContent: "center" }}>
@@ -36,8 +54,9 @@ const Match = (props) => {
                 <Button variant="contained" color="primary" onClick={handleSubmit} sx={{width: "150px", height:"50px"}}>
                 Submit
                 </Button>
+                <Box sx={{height: "200px"}}></Box>
+                <Box>Timer Left: {timeLeft}</Box>
         </FormControl>
-        
     </Box>);
 }
 
