d746f564ba514debf0f0fa26f10bc9863b324138
LowJF <lowjianfeng@hotmail.com>
2023-10-12 00:40:09 +0800

    Add functionality to generate room id Remove functionality that syncs room (temporary)

--- backend/collab-backend/index.js
+++ backend/collab-backend/index.js
@@ -1,42 +1,80 @@
+const express = require("express")
+const cors = require("cors")
+const { v4: uuidv4 } = require('uuid');
 const mongoose = require("mongoose")
 const Document = require("./Document")
 
-mongoose.connect("mongodb://localhost/document_db")
-
-const io = require('socket.io')(9000, {
-    cors : {
-        origin: 'http://localhost:3000',
-        methods: ["GET", "POST"]
-    }
-})
-
+const PORT = 9000
 const defaultDocumentData = ""
 
-io.on("connection", socket => {
-    socket.on('get-document', async documentID => {
-        // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
-        const document = await findOrCreateDocument(documentID)
-        socket.join(documentID)
-        socket.emit("load-document", document.data)
+mongoose.connect("mongodb://localhost/document_db")
 
-        // Proprogate changes from users to other users in the same channel
-        socket.on("send-changes", delta => {
-            socket.broadcast.to(documentID).emit("recieve-changes", delta)
-        })
-        
-        // Save document into MongoDB
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentID, {data})
-            findOrCreateDocument(documentID)
-        })
-    })
+const app = express()
+const server = require("http").createServer(app)
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST"],
+    optionsSuccessStatus: 200
+}))
+app.use(express.json())
+const io = require("socket.io")(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ["GET", "POST"]
+    }
+})
+server.listen(PORT, () => {
+    console.log(`Collab service connected on port ${PORT}`);
+});
+
+app.post("/get_room_id", async function (req, res) {
+    // User ids are not used. Could have a future use.
+    const {user1, user2} = req.body
+
+    var room_id = null
+    var document = null
+
+    do {
+        room_id = uuidv4()
+        console.log(room_id)
+        document = await Document.findById(room_id)
+    } while (document)
+
+    res.send({ room_id : room_id})
 })
 
-async function findOrCreateDocument(id) {
-    if (id == null) return
+// const io = require('socket.io')(PORT, {
+//     cors : {
+//         origin: 'http://localhost:3000',
+//         methods: ["GET", "POST"]
+//     }
+// })
+
+// io.on("connection", socket => {
+//     socket.on('get-document', async documentID => {
+//         // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
+//         const document = await findOrCreateDocument(documentID)
+//         socket.join(documentID)
+//         socket.emit("load-document", document.data)
+
+//         // Proprogate changes from users to other users in the same channel
+//         socket.on("send-changes", delta => {
+//             socket.broadcast.to(documentID).emit("recieve-changes", delta)
+//         })
+        
+//         // Save document into MongoDB
+//         socket.on("save-document", async data => {
+//             await Document.findByIdAndUpdate(documentID, {data})
+//             findOrCreateDocument(documentID)
+//         })
+//     })
+// })
+
+// async function findOrCreateDocument(id) {
+//     if (id == null) return
     
-    const document = await Document.findById(id)
-    if (document) return document 
-    return await Document.create({_id: id, data: defaultDocumentData})
+//     const document = await Document.findById(id)
+//     if (document) return document 
+//     return await Document.create({_id: id, data: defaultDocumentData})
     
-}
+// }
