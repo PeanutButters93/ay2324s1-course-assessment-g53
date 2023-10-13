86c630051d9b0796f9fde40d48c6227f86ad0bd4
LowJF <lowjianfeng@hotmail.com>
2023-10-14 02:08:20 +0800

    Removed Mongo DB related functionality for step-by-step containerization

--- backend/collab-backend/index.js
+++ backend/collab-backend/index.js
@@ -1,72 +1,76 @@
 const express = require("express")
 const cors = require("cors")
 const { v4: uuidv4 } = require('uuid');
-const mongoose = require("mongoose")
-const Document = require("./Document")
+// const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+// const Document = require("./Document")
 
-const PORT = 9000
+dotenv.config({
+    path: ".env.local"
+})
 const defaultDocumentData = ""
 
-mongoose.connect("mongodb://localhost/document_db")
+// mongoose.connect(process.env.MONGODB_URI)
 
 const app = express()
 const server = require("http").createServer(app)
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ["GET", "POST"],
     optionsSuccessStatus: 200
 }))
 app.use(express.json())
 const io = require("socket.io")(server, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: '*',
       methods: ["GET", "POST"]
     }
 })
-server.listen(PORT, () => {
-    console.log(`Collab service connected on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+    console.log(`Collab service connected on port ${process.env.PORT}`);
 });
 
 app.post("/get_room_id", async (req, res) => {
     // User ids are not used. Could have a future use.
-    const {user1, user2} = req.body
+    // const {user1, user2} = req.body
 
-    var room_id = null
-    var document = null
+    // var room_id = null
+    // var document = null
 
-    do {
-        room_id = uuidv4()
-        document = await Document.findById(room_id)
-    } while (document)
+    // do {
+    //     room_id = uuidv4()
+    //     document = await Document.findById(room_id)
+    // } while (document)
 
-    res.send({ room_id : room_id})
+    // res.send({ room_id : room_id})
+    res.send({ room_id : uuidv4()})
 })
 
-io.on("connection", socket => {
-    socket.on('get-document', async documentID => {
-        // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
-        const document = await findOrCreateDocument(documentID)
-        socket.join(documentID)
-        socket.emit("load-document", document.data)
+// io.on("connection", socket => {
+//     socket.on('get-document', async documentID => {
+//         // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
+//         const document = await findOrCreateDocument(documentID)
+//         socket.join(documentID)
+//         socket.emit("load-document", document.data)
 
-        // Proprogate changes from users to other users in the same channel
-        socket.on("send-changes", delta => {
-            socket.broadcast.to(documentID).emit("recieve-changes", delta)
-        })
+//         // Proprogate changes from users to other users in the same channel
+//         socket.on("send-changes", delta => {
+//             socket.broadcast.to(documentID).emit("recieve-changes", delta)
+//         })
         
-        // Save document into MongoDB
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentID, {data})
-            findOrCreateDocument(documentID)
-        })
-    })
-})
+//         // Save document into MongoDB
+//         socket.on("save-document", async data => {
+//             await Document.findByIdAndUpdate(documentID, {data})
+//             findOrCreateDocument(documentID)
+//         })
+//     })
+// })
 
-async function findOrCreateDocument(id) {
-    if (id == null) return
+// async function findOrCreateDocument(id) {
+//     if (id == null) return
     
-    const document = await Document.findById(id)
-    if (document) return document 
-    return await Document.create({_id: id, data: defaultDocumentData})
+//     const document = await Document.findById(id)
+//     if (document) return document 
+//     return await Document.create({_id: id, data: defaultDocumentData})
     
-}
+// }
