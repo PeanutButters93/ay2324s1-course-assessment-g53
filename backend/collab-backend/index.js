eed0086bfc981a8554ad89944757f9800674b510
LowJF <lowjianfeng@hotmail.com>
2023-10-14 03:50:43 +0800

    Reimplemented deleted features

--- backend/collab-backend/index.js
+++ backend/collab-backend/index.js
@@ -45,31 +45,31 @@ app.post("/get_room_id", async (req, res) => {
     res.send({ room_id : room_id})
 })
 
-// io.on("connection", socket => {
-//     socket.on('get-document', async documentID => {
-//         // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
-//         const document = await findOrCreateDocument(documentID)
-//         socket.join(documentID)
-//         socket.emit("load-document", document.data)
+io.on("connection", socket => {
+    socket.on('get-document', async documentID => {
+        // Upon connecting, grab latest copy of document w/ ID, and join channel w/ ID
+        const document = await findOrCreateDocument(documentID)
+        socket.join(documentID)
+        socket.emit("load-document", document.data)
 
-//         // Proprogate changes from users to other users in the same channel
-//         socket.on("send-changes", delta => {
-//             socket.broadcast.to(documentID).emit("recieve-changes", delta)
-//         })
+        // Proprogate changes from users to other users in the same channel
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentID).emit("recieve-changes", delta)
+        })
         
-//         // Save document into MongoDB
-//         socket.on("save-document", async data => {
-//             await Document.findByIdAndUpdate(documentID, {data})
-//             findOrCreateDocument(documentID)
-//         })
-//     })
-// })
+        // Save document into MongoDB
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentID, {data})
+            findOrCreateDocument(documentID)
+        })
+    })
+})
 
-// async function findOrCreateDocument(id) {
-//     if (id == null) return
+async function findOrCreateDocument(id) {
+    if (id == null) return
     
-//     const document = await Document.findById(id)
-//     if (document) return document 
-//     return await Document.create({_id: id, data: defaultDocumentData})
+    const document = await Document.findById(id)
+    if (document) return document 
+    return await Document.create({_id: id, data: defaultDocumentData})
     
-// }
+}
