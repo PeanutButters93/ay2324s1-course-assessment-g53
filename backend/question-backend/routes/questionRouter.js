2d703e0f32e9550c0db786b28e675d4cf5900d1b
kohkaixun <kkx1929@gmail.com>
2023-10-19 01:18:10 +0800

    Update questionRouter.js

--- backend/question-backend/routes/questionRouter.js
+++ backend/question-backend/routes/questionRouter.js
@@ -1,19 +1,18 @@
 import express from "express";
+
+import { checkLogin } from "../middleware/checkLogin.js";
+import { checkAdmin } from "../middleware/checkAdmin.js";
+
 import { getQuestions } from "../controller/getQuestions.js";
 import { addQuestion } from "../controller/addQuestion.js";
 import { updateQuestion } from "../controller/updateQuestion.js";
 import { deleteQuestion } from "../controller/deleteQuestion.js";
-import { checkLogin } from "../middleware/checkLogin.js";
-import { checkAdmin } from "../middleware/checkAdmin.js";
+
 const router = express.Router();
 
-// Question routes
 router.get("/", [checkLogin], getQuestions);
 router.post("/", [checkLogin, checkAdmin], addQuestion);
 router.put("/", [checkLogin, checkAdmin], updateQuestion);
 router.delete("/:id", [checkLogin, checkAdmin], deleteQuestion);
-
-// Category Routes
-router.get("/categories", getQuestions);
 
 export default router;
