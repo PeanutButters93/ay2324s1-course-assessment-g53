5934007137aefcc68e5b59a2c157c98a22c0a1ff
kohkaixun <kkx1929@gmail.com>
2023-10-19 01:30:13 +0800

    Add addCategory to categoryRouter.js

--- backend/question-backend/routes/categoryRouter.js
+++ backend/question-backend/routes/categoryRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { getCategories } from "../controller/getCategories.js";
+import { addCategory } from "../controller/addCategory.js";
 
 const router = express.Router();
 
 router.get("/", getCategories);
+router.post("/", addCategory);
 
 export default router;
