7aa4fff3cc1e2470a81b4d172f46ea1483b898af
kohkaixun <kkx1929@gmail.com>
2023-10-19 19:34:55 +0800

    Add updateCategory to categoryRouter.js

--- backend/question-backend/routes/categoryRouter.js
+++ backend/question-backend/routes/categoryRouter.js
@@ -3,11 +3,13 @@ import express from "express";
 import { getCategories } from "../controller/getCategories.js";
 import { addCategory } from "../controller/addCategory.js";
 import { deleteCategory } from "../controller/deleteCategory.js";
+import { updateCategory } from "../controller/updateCategory.js";
 
 const router = express.Router();
 
 router.get("/", getCategories);
 router.post("/", addCategory);
+router.put("/", updateCategory);
 router.delete("/:id", deleteCategory);
 
 export default router;
