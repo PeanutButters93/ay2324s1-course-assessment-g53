34a9b5032e3324123750f60d29e316731d7c106c
zzthian <zzthian@hotmail.com>
2023-10-19 14:48:15 +0800

    Buggy version

--- frontend/src/components/VideoCall.js
+++ frontend/src/components/VideoCall.js
@@ -19,7 +19,6 @@ const VideoCall = () => {
     setSocket(s);
     
     peer.on("open", (id) => {
-        console.log("peer opened")
       s.emit("join-room", ROOM_ID, id , () => {
         setPeerId(id)
       });
@@ -43,7 +42,10 @@ const VideoCall = () => {
         audio: true,
       })
       .then((stream) => {
-        addVideoStream(personalVideo, stream);
+        if (videoElements.length == 0) {
+        console.log("Adding personal video")
+        addVideoStream(personalVideo, stream, "personal");
+        }
         socket.emit("video-ready", {"ping" : "pong"}); 
         //this is to allow any person who calls me to get my video stream, and for me to get theirs
         peer.on("call", (call, otherStream) => {
@@ -52,18 +54,25 @@ const VideoCall = () => {
         //   call.on("stream", (userVideoStream) => {
         //     addVideoStream(null, userVideoStream);
         //   });
-        addVideoStream(null, otherStream)
+        console.log("otherStream: ", otherStream)
+        if (otherStream === undefined) return
+        const video = document.createElement("video")
+        // console.log("adding other guy video")
+         addVideoStream(video, otherStream, "Other")
         });
         //Server will emit this to the room, this is to allow for new user connected, I call the new user
 
         socket.on("user-connected", (userId) => {
             if (userId === peerId) return
-          console.log("Trying to connect to: ", userId);
+            console.log("calling the other guy")
           connectToNewUser(userId, stream);
         });
 
         socket.on('user-disconnected', userId => {
-            if (peers[userId]) peers[userId].close()
+            if (peers[userId]){
+                peers[userId].close()
+            } 
+            
           })
       });
   }, [peerId]);
@@ -73,10 +82,11 @@ const VideoCall = () => {
     //when I am calling this user, I send over my stream
     const call = peer.call(userId, stream);
     const video = document.createElement("video");
+    
     // I then also expect a stream in return which I would then use to add to my display
-    call.on("stream", (userVideoStream) => {
+    call.once("stream", (userVideoStream) => {
       console.log("hi")
-      addVideoStream(video, userVideoStream);
+      addVideoStream(video, userVideoStream, "Other person");
     });
     //when the call is closed then I will remove the video stream
     call.on("close", () => {
@@ -86,21 +96,14 @@ const VideoCall = () => {
     peers[userId] = call;
   }
 
-  function addVideoStream(video, stream) {
-    if (video !== null) {
+  function addVideoStream(video, stream, whose) {
+      console.log(whose)
       video.srcObject = stream;
       video.addEventListener("loadedmetadata", () => {
         video.play();
       });
       setVideoElements((prevVideoElements) => [...prevVideoElements, video]);
       return;
-    }
-    const newVideo = document.createElement("video");
-    newVideo.srcObject = stream;
-    newVideo.addEventListener("loadedmetadata", () => {
-      newVideo.play();
-    });
-    setVideoElements((prevVideoElements) => [...prevVideoElements, newVideo]);
   }
 
 
@@ -112,17 +115,21 @@ const VideoCall = () => {
 
       wrapper.innerHTML = "";
       const editor = document.createElement("div");
-      const textNode = document.createTextNode("Hello world");
-      editor.appendChild(textNode);
+    //   const textNode = document.createTextNode("Hello world");
+    //   editor.appendChild(textNode);
 
-      videoElements.forEach((video) => editor.appendChild(video));
+      videoElements.forEach((video) => {
+        video.style.width = '250px'
+        video.style.height = '250px'
+        video.style.margin = '10px'
+        editor.appendChild(video)});
       console.log(videoElements);
       wrapper.append(editor);
     },
     [videoElements]
   );
 
-  return <div id="video" ref={wrapperRef}>
+  return <div id="video" ref={wrapperRef} style={{ width: '750px', backgroundColor: 'white'}}>
   </div>;
 };
 
