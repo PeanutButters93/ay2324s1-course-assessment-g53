e530bc96170d47cbd2cb75674695959079b57ac4
kohkaixun <kkx1929@gmail.com>
2023-10-19 12:52:03 +0800

    Add deleteCategory to categoryRouter.js Delete using mongodb _id field

--- backend/question-backend/routes/categoryRouter.js
+++ backend/question-backend/routes/categoryRouter.js
@@ -2,10 +2,12 @@ import express from "express";
 
 import { getCategories } from "../controller/getCategories.js";
 import { addCategory } from "../controller/addCategory.js";
+import { deleteCategory } from "../controller/deleteCategory.js";
 
 const router = express.Router();
 
 router.get("/", getCategories);
 router.post("/", addCategory);
+router.delete("/:id", deleteCategory);
 
 export default router;
