a6d7dbfb84bb2a940202531917c88ddc40d596a6
zzthian <zzthian@hotmail.com>
2023-10-21 20:58:50 +0800

    Shift video location to a better position

--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Grid, Paper, Typography, Button } from '@mui/material';
-import TextEditor from '../components/TextEditor';
-import DividerIcon from '@mui/icons-material/DragHandle';
-import LogEditorButton from '../components/LogEditorButton';
-import VideoCall from '../components/VideoCall';
+import React, { useState, useEffect } from "react";
+import { Grid, Paper, Typography, Button } from "@mui/material";
+import TextEditor from "../components/TextEditor";
+import DividerIcon from "@mui/icons-material/DragHandle";
+import LogEditorButton from "../components/LogEditorButton";
+import VideoCall from "../components/VideoCall";
 
 const Room = () => {
   const [dividerPosition, setDividerPosition] = useState(50);
   const [isDragging, setIsDragging] = useState(false);
 
   const handleDividerDrag = (e) => {
-    const newDividerPosition = Math.max(20, Math.min((e.clientX / window.innerWidth) * 100, 80)); // Limiting the movement between 20% and 80%
+    const newDividerPosition = Math.max(
+      20,
+      Math.min((e.clientX / window.innerWidth) * 100, 80)
+    ); // Limiting the movement between 20% and 80%
     setDividerPosition(newDividerPosition);
   };
 
@@ -18,75 +21,103 @@ const Room = () => {
     const handleMouseUp = () => setIsDragging(false);
 
     if (isDragging) {
-      document.addEventListener('mousemove', handleDividerDrag);
-      document.addEventListener('mouseup', handleMouseUp);
+      document.addEventListener("mousemove", handleDividerDrag);
+      document.addEventListener("mouseup", handleMouseUp);
     }
 
     return () => {
-      document.removeEventListener('mousemove', handleDividerDrag);
-      document.removeEventListener('mouseup', handleMouseUp);
+      document.removeEventListener("mousemove", handleDividerDrag);
+      document.removeEventListener("mouseup", handleMouseUp);
     };
   }, [isDragging, handleDividerDrag]);
 
   return (
     <div>
-    <Grid container spacing={3} style={{ position: 'relative', overflow: 'hidden' }}>
-      
-      {/* Problem Description Section */}
-      <Grid item style={{ flex: `0 0 ${dividerPosition}%`, position: 'relative' }}>
-        <Paper elevation={3} style={{ padding: '16px', height: '100%' }}>
-          <Typography variant="h5">Problem Title</Typography>
-          <Typography variant="subtitle1" color="textSecondary">Difficulty: Easy</Typography>
-          <Typography variant="body1" paragraph>
-            Here is the problem description. It should explain what the problem is
-            and what the user is expected to do in order to solve it.
-          </Typography>
-        </Paper>
-      </Grid>
-      <VideoCall/>
-      
-      {/* Divider */}
-      <div
-        style={{
-          width: '2px',
-          background: '#ccc',
-          cursor: 'col-resize',
-          position: 'absolute',
-          top: 0,
-          bottom: 0,
-          left: `calc(${dividerPosition}% - 1px)`,
-          display: 'flex',
-          alignItems: 'center',
-          zIndex: 1,
-        }}
-        onMouseDown={(e) => {
-          e.preventDefault();
-          setIsDragging(true);
-        }}
+      <Grid
+        container
+        spacing={3}
+        style={{ position: "relative", overflow: "hidden" }}
       >
-        <DividerIcon style={{ fontSize: '16px', color: '#666' }} />
-      </div>
-
-      {/* Playground and Submit Section */}
-      <Grid item style={{ flex: `0 0 ${100 - dividerPosition}%` }}>
-        <Paper elevation={3} style={{ height: 'calc(100vh - 64px)', display: 'flex', flexDirection: 'column' }}>
-          
-          {/* Playground */}
-          <div style={{ flex: 1, padding: '16px', overflowY: 'auto', overflowX: 'hidden', whiteSpace: 'pre-wrap', wordWrap: 'break-word', maxHeight: 'calc(100vh - 64px - 68px)'}}>
-            <Typography variant="h6">Playground</Typography>
-            <TextEditor />
-          </div>
-          
-          {/* Submit Section */}
-          <Paper elevation={3} style={{ padding: '16px' }}>
-            <Button variant="contained" color="primary">Submit</Button>
+        {/* Problem Description Section */}
+        <Grid
+          item
+          style={{ flex: `0 0 ${dividerPosition}%`, position: "relative" }}
+        >
+          <Paper elevation={3} style={{ padding: "16px", height: "60%" }}>
+            <Typography variant="h5">Problem Title</Typography>
+            <Typography variant="subtitle1" color="textSecondary">
+              Difficulty: Easy
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Here is the problem description. It should explain what the
+              problem is and what the user is expected to do in order to solve
+              it.
+            </Typography>
           </Paper>
-        </Paper>
+          <Paper elevation={4} style={{ padding: "16px", height: "40%" }}>
+            <VideoCall />
+          </Paper>
+        </Grid>
+
+        {/* Divider */}
+        <div
+          style={{
+            width: "2px",
+            background: "#ccc",
+            cursor: "col-resize",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: `calc(${dividerPosition}% - 1px)`,
+            display: "flex",
+            alignItems: "center",
+            zIndex: 1,
+          }}
+          onMouseDown={(e) => {
+            e.preventDefault();
+            setIsDragging(true);
+          }}
+        >
+          <DividerIcon style={{ fontSize: "16px", color: "#666" }} />
+        </div>
+
+        {/* Playground and Submit Section */}
+        <Grid item style={{ flex: `0 0 ${100 - dividerPosition}%` }}>
+          <Paper
+            elevation={3}
+            style={{
+              height: "calc(100vh - 64px)",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            {/* Playground */}
+            <div
+              style={{
+                flex: 1,
+                padding: "16px",
+                overflowY: "auto",
+                overflowX: "hidden",
+                whiteSpace: "pre-wrap",
+                wordWrap: "break-word",
+                maxHeight: "calc(100vh - 64px - 68px)",
+              }}
+            >
+              <Typography variant="h6">Playground</Typography>
+              <TextEditor />
+            </div>
+
+            {/* Submit Section */}
+            <Paper elevation={3} style={{ padding: "16px" }}>
+              <Button variant="contained" color="primary">
+                Submit
+              </Button>
+            </Paper>
+          </Paper>
+        </Grid>
       </Grid>
 
-    </Grid>
-    
-    <LogEditorButton/>
+      <LogEditorButton />
     </div>
   );
 };
