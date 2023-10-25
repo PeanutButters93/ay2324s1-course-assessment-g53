ed5a3845c5e809cdc5198b8cf7fd702dbe178f50
kohkaixun <kkx1929@gmail.com>
2023-10-25 13:18:36 +0800

    Add comments in index.js for question backend

--- backend/question-backend/index.js
+++ backend/question-backend/index.js
@@ -20,7 +20,7 @@ mongoose.connect(uri);
 
 app.use("/api/questions", QuestionRouter);
 app.use("/api/categories", CategoryRouter);
-app.use("/", (req, res) => res.status(200).json({status: "OK"}))
+app.use("/", (req, res) => res.status(200).json({status: "OK"})) // health check, to see if can be removed
 app.listen(port, () => {
   console.log(`Question service connected on port ${port}`);
   console.log(process.env.USER_HOST)
