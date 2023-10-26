7d2d81219873fad741aa311d81038a4b530907fa
Darren Chang <darrenchang2@gmail.com>
2023-10-26 17:02:28 +0800

    Created socket for questions and added getQuestionByComplexitiy file

--- backend/question-backend/routes/questionRouter.js
+++ backend/question-backend/routes/questionRouter.js
@@ -5,6 +5,7 @@ import { updateQuestion } from "../controller/updateQuestion.js";
 import { deleteQuestion } from "../controller/deleteQuestion.js";
 import { checkLogin } from "../middleware/checkLogin.js";
 import { checkAdmin } from "../middleware/checkAdmin.js";
+import { getQuestionsByComplexity } from "../controller/getQuestionByComplexitiy.js";
 const router = express.Router();
 router.get("/", getQuestions);
 
@@ -13,5 +14,7 @@ router.post("/", [checkLogin, checkAdmin], addQuestion);
 router.put("/", [checkLogin, checkAdmin], updateQuestion);
 
 router.delete("/:id", [checkLogin, checkAdmin], deleteQuestion);
+
+router.get("/:complexity", getQuestionsByComplexity);
 
 export default router;
