19dcb30c0fd692b7f260be7f74e46598addd21bd
Lu Chenyu <62133038+adam07018@users.noreply.github.com>
2023-10-30 14:32:40 +0800

    Revert "Room.js retrieves questions from the question database"

--- frontend/src/pages/Room.js
+++ frontend/src/pages/Room.js
@@ -4,154 +4,92 @@ import TextEditor from "../components/TextEditor";
 import DividerIcon from "@mui/icons-material/DragHandle";
 import LogEditorButton from "../components/LogEditorButton";
 import VideoCall from "../components/VideoCall";
-import { useParams, useLocation } from "react-router-dom";
-import { io } from "socket.io-client";
 
 const Room = () => {
-  const [dividerPosition, setDividerPosition] = useState(50);
-  const [isDragging, setIsDragging] = useState(false);
-  const [question, setQuestion] = useState(null);
-  const { roomId } = useParams();
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const difficulty = queryParams.get("difficulty");
-  const COLLAB_HOST = process.env.REACT_APP_COLLAB_HOST
-    ? process.env.REACT_APP_COLLAB_HOST
-    : "http://localhost:9000";
-  const socket = io(COLLAB_HOST);
+  const [dividerPosition, setDividerPosition] = useState(50)
+  const [isDragging, setIsDragging] = useState(false)
 
   const handleDividerDrag = (e) => {
-    const newDividerPosition = Math.max(
-      20,
-      Math.min((e.clientX / window.innerWidth) * 100, 80)
-    ); // Limiting the movement between 20% and 80%
-    setDividerPosition(newDividerPosition);
-  };
+    const newDividerPosition = Math.max(20, Math.min((e.clientX / window.innerWidth) * 100, 80)) // Limiting the movement between 20% and 80%
+    setDividerPosition(newDividerPosition)
+  }
 
   useEffect(() => {
-    const handleMouseUp = () => setIsDragging(false);
+    const handleMouseUp = () => setIsDragging(false)
 
     if (isDragging) {
-      document.addEventListener("mousemove", handleDividerDrag);
-      document.addEventListener("mouseup", handleMouseUp);
+      document.addEventListener('mousemove', handleDividerDrag)
+      document.addEventListener('mouseup', handleMouseUp)
     }
-
-    if (!question) {
-
-    // Emit event to join the room
-    socket.emit('join-question-room', roomId);
-    
-    // Emit event to request questions
-    socket.emit("request-questions", roomId, difficulty);
-    }
-
-    // Listen for the response with the question data
-    const handleReceiveQuestions = (data) => {
-      console.log("handlereceived reached")
-      setQuestion(data);
-    };
-    socket.on('receive-questions', handleReceiveQuestions);
 
     return () => {
-      document.removeEventListener("mousemove", handleDividerDrag);
-      document.removeEventListener("mouseup", handleMouseUp);
-      // Remove the socket listener
-      socket.off("receive-questions", handleReceiveQuestions);
-    };
-  }, [isDragging, handleDividerDrag, roomId, difficulty]);
+      document.removeEventListener('mousemove', handleDividerDrag)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+  }, [isDragging, handleDividerDrag])
 
   return (
-    <Grid
-      container
-      spacing={3}
-      style={{ position: "relative", overflow: "hidden" }}
-    >
+    <Grid container spacing={3} style={{ position: 'relative', overflow: 'hidden' }}>
+
       {/* Problem Description Section */}
-      <Grid
-        item
-        style={{ flex: `0 0 ${dividerPosition}%`, position: "relative" }}
-      >
-        <Paper elevation={3} style={{ padding: "16px", height: "70%" }}>
+      <Grid item style={{ flex: `0 0 ${dividerPosition}%`, position: 'relative' }}>
+        <Paper elevation={3} style={{ padding: '16px', height: '70%' }}>
           <Typography variant="h5">Problem Title</Typography>
-          <Typography variant="h5">
-            {/* Rendering the title directly */}
-            {question?.title || "Loading title..."}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            Difficulty: {difficulty}
-          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">Difficulty: Easy</Typography>
           <Typography variant="body1" paragraph>
-            {question?.description || "Loading description..."}
+            Here is the problem description. It should explain what the problem is
+            and what the user is expected to do in order to solve it.
           </Typography>
-          <Paper />
+          <Paper/>
+          
         </Paper>
-        <Paper style={{ margin: "10px", height: "30%" }}>
-          <VideoCall />
+        <Paper style={{ margin:"10px", height: "30%"}}>
+          <VideoCall/>
         </Paper>
       </Grid>
 
       {/* Divider */}
       <div
         style={{
-          width: "2px",
-          background: "#ccc",
-          cursor: "col-resize",
-          position: "absolute",
+          width: '2px',
+          background: '#ccc',
+          cursor: 'col-resize',
+          position: 'absolute',
           top: 0,
           bottom: 0,
           left: `calc(${dividerPosition}% - 1px)`,
-          display: "flex",
-          alignItems: "center",
+          display: 'flex',
+          alignItems: 'center',
           zIndex: 1,
         }}
         onMouseDown={(e) => {
-          e.preventDefault();
-          setIsDragging(true);
+          e.preventDefault()
+          setIsDragging(true)
         }}
       >
-        <DividerIcon style={{ fontSize: "16px", color: "#666" }} />
+        <DividerIcon style={{ fontSize: '16px', color: '#666' }} />
       </div>
 
       {/* Playground and Submit Section */}
-      <Grid
-        item
-        style={{
-          flex: `0 0 ${100 - dividerPosition}%`,
-          maxWidth: `${100 - dividerPosition}%`,
-        }}
-      >
-        <Paper
-          elevation={3}
-          style={{
-            height: "calc(100vh - 64px)",
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
+      <Grid item style={{ flex: `0 0 ${100 - dividerPosition}%` , maxWidth: `${100 - dividerPosition}%`}}>
+        <Paper elevation={3} style={{ height: 'calc(100vh - 64px)', display: 'flex', flexDirection: 'column' }}>
+
           {/* Playground */}
-          <div
-            style={{
-              flex: 1,
-              padding: "16px",
-              overflowY: "auto",
-              overflowX: "auto",
-              whiteSpace: "pre-wrap",
-              wordWrap: "break-word",
-              maxHeight: "calc(100vh - 64px - 68px)",
-            }}
-          >
+          <div style={{ flex: 1, padding: '16px', overflowY: 'auto', overflowX: 'auto', whiteSpace: 'pre-wrap', wordWrap: 'break-word', maxHeight: 'calc(100vh - 64px - 68px)' }}>
             <Typography variant="h6">Playground</Typography>
             <TextEditor />
           </div>
 
           {/* Submit Section */}
-          <Paper elevation={3} style={{ padding: "16px" }}>
+          <Paper elevation={3} style={{ padding: '16px' }}>
             <LogEditorButton />
           </Paper>
+          
         </Paper>
       </Grid>
-    </Grid>
-  );
-};
 
-export default Room;
+    </Grid>
+  )
+}
+
+export default Room
