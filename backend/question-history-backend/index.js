492c8dc71a37ae25a857ab7bf634eec0b2d1da9e
LowJF <lowjianfeng@hotmail.com>
2023-10-31 12:07:57 +0800

    QuestionHistoryPage : Add a backend for future implementation

---
+++ backend/question-history-backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+
+const historyRouter = require('./routes/historyRouter')
+dotenv.config({
+    path: ".env.local"
+})
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+
+app.use("/api/history", historyRouter)
+app.use("/", (req, res) => res.status(200).json({status: "OK"}))
+app.listen(PORT, () => {
+    console.log(`Question history service connected on port ${PORT}`);
+});
--- frontend/src/pages/QuestionHistoryPage.js
+++ frontend/src/pages/QuestionHistoryPage.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import useCookie from "../components/useCookie"
+
+const HISTORY_HOST = process.env.REACT_APP_HISTORY_HOST ? process.env.REACT_APP_HISTORY_HOST : "http://localhost:5000/api/history"
+
 const QuestionHistoryPage = () => {
-    return <div>welcome to the qsn history page</div>
+    const [data, setData] = useState("welcome to the qsn history page")
+    const {getAuthCookie} = useCookie()
+
+    useEffect(() => {
+        const response  = axios.post(HISTORY_HOST, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getAuthCookie()
+            }, })
+            .then(response => {
+                if (response.status === 403) {
+                  // Handle the forbidden error
+                  return response.then(err => { throw err })
+                }
+                setData(response.data.document)
+              })
+            .catch(error => {
+                console.error('Error:', error);
+                return;
+            })
+
+        
+    }, [])
+    
+
+    return <div>~~ {data} ~~</div>
 }
 
 export default QuestionHistoryPage
