9be082fd8ffdfdfac64db688b710ddda2b0acb4f
LowJF <lowjianfeng@hotmail.com>
2023-10-31 12:27:09 +0800

    Containerize Question History BE

--- frontend/src/pages/QuestionHistoryPage.js
+++ frontend/src/pages/QuestionHistoryPage.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import useCookie from "../components/useCookie"
 
-const HISTORY_HOST = process.env.REACT_APP_HISTORY_HOST ? process.env.REACT_APP_HISTORY_HOST : "http://localhost:5000/api/history"
+const REACT_APP_HISTORY_HOST = process.env.REACT_APP_HISTORY_HOST ? process.env.REACT_APP_HISTORY_HOST : "http://localhost:5000/api/history"
 
 const QuestionHistoryPage = () => {
     const [data, setData] = useState("welcome to the qsn history page")
     const {getAuthCookie} = useCookie()
 
     useEffect(() => {
-        const response  = axios.post(HISTORY_HOST, {
+        const response  = axios.post(REACT_APP_HISTORY_HOST, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': getAuthCookie()
