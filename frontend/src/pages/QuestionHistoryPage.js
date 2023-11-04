6eaec00c62ab724ff16b902ccc097e434f77a62c
Darren Chang <darrenchang2@gmail.com>
2023-11-04 23:17:40 +0800

    Revamp Question History page

--- frontend/src/pages/QuestionHistoryPage.js
+++ frontend/src/pages/QuestionHistoryPage.js
@@ -1,36 +1,66 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import useCookie from "../components/useCookie"
+import React, { useState, useEffect } from 'react';
+import { Modal, Box, Typography, Button } from '@mui/material';
+import axios from 'axios';
 
-const REACT_APP_HISTORY_HOST = process.env.REACT_APP_HISTORY_HOST ? process.env.REACT_APP_HISTORY_HOST : "http://localhost:5000/api/history"
+const HISTORY_HOST = process.env.REACT_APP_HISTORY_HOST || "http://localhost:8000/api/history";
 
-const QuestionHistoryPage = () => {
-    const [data, setData] = useState("welcome to the qsn history page")
-    const {getAuthCookie} = useCookie()
+function QuestionHistoryPage() {
+  const [history, setHistory] = useState([]);
+  const [selectedQuestion, setSelectedQuestion] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    useEffect(() => {
-        const response  = axios.post(REACT_APP_HISTORY_HOST, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': getAuthCookie()
-            }, })
-            .then(response => {
-                if (response.status === 403) {
-                  // Handle the forbidden error
-                  return response.then(err => { throw err })
-                }
-                setData(response.data.document)
-              })
-            .catch(error => {
-                console.error('Error:', error);
-                return;
-            })
+  useEffect(() => {
+    async function fetchHistory() {
+      try {
+        const response = await axios.get(HISTORY_HOST);
+        setHistory(response.data);
+      } catch (error) {
+        console.error('Error fetching history:', error);
+      }
+    }
 
-        
-    }, [])
-    
+    fetchHistory();
+  }, []);
 
-    return <div>~~ {data} ~~</div>
+  const handleOpenModal = (question) => {
+    setSelectedQuestion(question);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      {history.map((question) => (
+        <div key={question._id}>
+          <h2>{question.question_title}</h2>
+          <p>{question.question_description}</p>
+          <p>Categories: {question.categories.join(', ')}</p>
+          <p>Complexity: {question.complexity}</p>
+          <p>Last Attempt: {new Date(question.last_attempt).toLocaleString()}</p>
+          <Button onClick={() => handleOpenModal(question)}>View Attempt</Button>
+        </div>
+      ))}
+
+      <Modal
+        open={isModalOpen}
+        onClose={handleCloseModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {selectedQuestion?.question_title}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {selectedQuestion?.attempt}
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
 }
 
-export default QuestionHistoryPage
+export default QuestionHistoryPage;
