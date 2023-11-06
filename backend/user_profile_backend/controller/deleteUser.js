8fef4f508694063b539f184e13c581dc6382b140
LowJF <lowjianfeng@hotmail.com>
2023-11-06 14:40:57 +0800

    deleteUser : Temporarily disable delete user feature

--- backend/user_profile_backend/controller/deleteUser.js
+++ backend/user_profile_backend/controller/deleteUser.js
@@ -5,16 +5,16 @@ const deleteUserByUserID = (request, response) => {
     const id = request.body.user_data.user_id;
     console.log(id);
 
-    pool.query('DELETE FROM users WHERE user_id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        if (results.rowCount === 0) {
-            response.status(404).send(`No user found with ID: ${id}`)
-        } else {
-            response.status(200).send(`User deleted with ID: ${id}`)
-        }
-    })
+    // pool.query('DELETE FROM users WHERE user_id = $1', [id], (error, results) => {
+    //     if (error) {
+    //         throw error
+    //     }
+    //     if (results.rowCount === 0) {
+    //         response.status(404).send(`No user found with ID: ${id}`)
+    //     } else {
+    //         response.status(200).send(`User deleted with ID: ${id}`)
+    //     }
+    // })
 }
 
 module.exports = {
