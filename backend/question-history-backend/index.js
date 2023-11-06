a60af04f0c435502555ed3898b1e48f7bfb22214
LowJF <lowjianfeng@hotmail.com>
2023-11-06 17:08:00 +0800

    Question History BE : Update add question to validate token on BE

--- backend/question-history-backend/index.js
+++ backend/question-history-backend/index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const dotenv = require("dotenv")
 const amqp = require("amqplib")
+const jwt = require("jsonwebtoken");
 
 const historyRouter = require('./routes/historyRouter')
 dotenv.config({
@@ -9,7 +10,8 @@ dotenv.config({
 })
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+const secretKey = "yourSecretKey"
 
 app.use(cors())
 
@@ -21,49 +23,72 @@ app.listen(PORT, () => {
 
 
 const RABBIT_MQ_HOST = process.env.RABBIT_MQ_HOST ? process.env.RABBIT_MQ_HOST : 'amqp://guest:guest@localhost:5672'
-const queueName = "questionHistoryAddEntry"
+const AddEntryQueue = "questionHistoryAddEntry"
+const DeleteUserQueue = "deleteUserQueue"
 
-async function startConsumer() {
+async function startAmqp() {
+  // Connect to RabbitMQ
+  const connection = await amqp.connect(RABBIT_MQ_HOST);
+  const channel = await connection.createChannel();
+  startAddEntryQueue(channel);
+  startDeleteUserQueue(channel);
+}
+
+
+
+async function startAddEntryQueue(channel) {
     try {
-      // Connect to RabbitMQ
-      const connection = await amqp.connect(RABBIT_MQ_HOST);
-      const channel = await connection.createChannel();
   
       // Declare a queue (must match the queue you want to consume from)
-      await channel.assertQueue(queueName);
+      await channel.assertQueue(AddEntryQueue);
   
       // Consume messages from the queue
-      channel.consume(queueName, (message) => {
+      channel.consume(AddEntryQueue, (message) => {
         if (message.content) {
           const messageData = JSON.parse(message.content.toString('utf-8'));
 
-          // TODO verification
-          console.log(messageData.user_cookie)
+          var token = messageData.user_cookie
+          
+          try {
+            const user_id = jwt.verify(token, secretKey).user_data.user_id;
+            console.log(user_id)
+            console.log(messageData.question)
+            console.log(messageData.attempt)
+            console.log(messageData.timestamp)
+          } catch (e) {
+            console.log(e)
+          } finally {
+            // Acknowledge the message to remove it from the queue
+            channel.ack(message);
+          }
 
-          const foo = message.content.toString('utf-8');
-
-          const tokenBody = foo.split('.')[1]
-          let buffer = JSON.parse(atob(tokenBody))
-          const user_id = buffer.user_data.user_id
-          console.log(user_id)
-
-          console.log(messageData.question)
-
-          console.log(messageData.attempt)
-          console.log(messageData.timestamp)
-
-          // console.log(user_id)
-          // Process the message as needed
-  
-          // Acknowledge the message to remove it from the queue
-          channel.ack(message);
         }
       });
   
-      console.log(`Consumer is listening for messages in the "${queueName}" queue.`);
+      console.log(`Consumer is listening for messages in the "${AddEntryQueue}" queue.`);
     } catch (error) {
       console.error('Error starting consumer:', error);
     }
 }
 
-startConsumer();
+async function startDeleteUserQueue(channel) {
+    try {
+  
+      // Declare a queue (must match the queue you want to consume from)
+      await channel.assertQueue(DeleteUserQueue);
+  
+      // Consume messages from the queue
+      channel.consume(DeleteUserQueue, (message) => {
+        if (message.content) {
+          const messageData = JSON.parse(message.content.toString('utf-8'));
+          console.log(messageData)
+        }
+      });
+  
+      console.log(`Consumer is listening for messages in the "${DeleteUserQueue}" queue.`);
+    } catch (error) {
+      console.error('Error starting consumer:', error);
+    }
+}
+
+startAmqp()
--- backend/user_profile_backend/controller/deleteUser.js
+++ backend/user_profile_backend/controller/deleteUser.js
@@ -1,20 +1,36 @@
 const pool = require('../database/db.js')
+const amq = require("amqplib")
 
-const deleteUserByUserID = (request, response) => {
+const RABBIT_MQ_HOST = process.env.RABBIT_MQ_HOST ? process.env.RABBIT_MQ_HOST : 'amqp://guest:guest@localhost:5672'
+const queueName = "deleteUserQueue"
+
+const deleteUserByUserID = async (request, response) => {
     console.log(request.body);
     const id = request.body.user_data.user_id;
     console.log(id);
 
-    // pool.query('DELETE FROM users WHERE user_id = $1', [id], (error, results) => {
-    //     if (error) {
-    //         throw error
-    //     }
-    //     if (results.rowCount === 0) {
-    //         response.status(404).send(`No user found with ID: ${id}`)
-    //     } else {
-    //         response.status(200).send(`User deleted with ID: ${id}`)
-    //     }
-    // })
+    const connection = await amq.connect(RABBIT_MQ_HOST)
+    const channel = await connection.createChannel()
+    try {
+        await channel.assertQueue(queueName)
+    } catch (error) {
+        console.log(error)
+    }
+
+    pool.query('DELETE FROM users WHERE user_id = $1', [id], (error, results) => {
+        if (error) {
+            throw error
+        }
+        if (results.rowCount === 0) {
+            response.status(404).send(`No user found with ID: ${id}`)
+        } else {
+            response.status(200).send(`User deleted with ID: ${id}`)
+            channel.sendToQueue(
+                queueName,
+                Buffer.from(JSON.stringify(id))
+            )
+        }
+    })
 }
 
 module.exports = {
