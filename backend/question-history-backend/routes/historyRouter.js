1ca35adacfe525958a72ddbd6150c885f4110186
PeanutButters93 <lowjianfeng@hotmail.com>
2023-11-07 21:28:47 +0800

    Fix not being able to connect to backend
    Removing the mongoose calls seem to solve this issue, unsure why

--- backend/question-history-backend/routes/historyRouter.js
+++ backend/question-history-backend/routes/historyRouter.js
@@ -2,13 +2,10 @@ const express = require("express");
 const router = express.Router();
 const dotenv = require("dotenv")
 const {getQuestionsHistory} = require("../controller/getUserQuestionHistory.js")
-const mongoose = require("mongoose");
 
 dotenv.config({
     path: ".env.local"
 })
-
-mongoose.connect(process.env.MONGODB_URI)
 
 router.get("/", getQuestionsHistory);
 
