9ae893a215e46311398589869b23c18b6af3e10d
PeanutButters93 <lowjianfeng@hotmail.com>
2023-11-07 21:25:31 +0800

    Temporarily disable getUserQuestionHistory to try and solve backend not sending issue

--- backend/question-history-backend/controller/getUserQuestionHistory.js
+++ backend/question-history-backend/controller/getUserQuestionHistory.js
@@ -4,20 +4,23 @@ const Question = require('../model/QuestionHistory');
 // Define the getQuestions function
 const getQuestionsHistory = async(req, res) => {
     try {
-        // Extract userID from the request parameters
-        const userID = req.params.userID;
+        res.send("hello from BE")
+        console.log("hello from BE")
+
+        // // Extract userID from the request parameters
+        // const userID = req.params.userID;
         
-        // Find questions by userID
-        const questions = await Question.find({ userid: userID });
-        console.log("backend", questions);
+        // // Find questions by userID
+        // const questions = await Question.find({ userid: userID });
+        // console.log("backend", questions);
 
-        // If no questions found, return 404
-        if (!questions || questions.length === 0) {
-            return res.status(404).send('No questions found for the given user.');
-        }
+        // // If no questions found, return 404
+        // if (!questions || questions.length === 0) {
+        //     return res.status(404).send('No questions found for the given user.');
+        // }
 
-        // Send back the list of questions
-        res.send(questions);
+        // // Send back the list of questions
+        // res.send(questions);
     } catch (err) {
         // Log the error and return a 500 status code
         console.error(err);
